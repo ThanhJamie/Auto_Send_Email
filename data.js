548fb3694b4f8016672334f24c6b752fcfc10e6c
JamieThanh <86048174+ThanhJamie@users.noreply.github.com>
2022-06-29 18:57:59 +0700

    Send_Auto_Email

---
+++ data.js
@@ -0,0 +1,218 @@
+const FEV_MEMBER_LIST = [{
+        "stt": 5,
+        "gender": "anh ",
+        "name": "Nguyễn Khai Trí",
+        "email": "Khaitri15@gmail.con"
+    },
+
+    {
+        "stt": 15,
+        "gender": "anh ",
+        "name": "Tô Thành Thuyền",
+        "email": "thanhthuyenco@gmail.com"
+    },
+    {
+        "stt": 22,
+        "gender": "anh ",
+        "name": "Trần Nguỵ Nhật Anh ",
+        "email": "anhtnnse62096@fpt.edu.vn"
+    },
+    {
+        "stt": 23,
+        "gender": "anh ",
+        "name": "Đoàn Trương Thiện Huy",
+        "email": "Thienhuy201266@gmail.con"
+    },
+    {
+        "stt": 24,
+        "gender": "anh ",
+        "name": "Diệp Phương Thanh",
+        "email": "thanhdpsb60999@fpt.edu.vn"
+    },
+
+    {
+        "stt": 29,
+        "gender": "anh ",
+        "name": "Lê Thịnh Phát",
+        "email": "PhatLTSB61096@fpt.edu.vn"
+    },
+    {
+        "stt": 32,
+        "gender": "anh ",
+        "name": "Nguyễn Hồng Anh",
+        "email": "ANHNHSE62597@FPT.EDU.VN"
+    },
+    {
+        "stt": 44,
+        "gender": "anh ",
+        "name": "Trần Công Sinh",
+        "email": "sinhtcse62637@fpt.edu.vn"
+    },
+    {
+        "stt": 56,
+        "gender": "anh ",
+        "name": "Nguyễn Hồng Anh Tuấn",
+        "email": "tuannhase62901@fpt.edu.vn"
+    },
+    {
+        "stt": 60,
+        "gender": "anh ",
+        "name": "Phan Minh Sung",
+        "email": "sungpmse63244@fpt.edu.vn"
+    },
+    {
+        "stt": 69,
+        "gender": "chị",
+        "name": "Đậu Thuý My",
+        "email": "mydtsb61397@fpt.edu.vn"
+    },
+    {
+        "stt": 71,
+        "gender": "chị",
+        "name": "Hồ Nguyễn Yến Nhi",
+        "email": "nhihnysb61440@fpt.edu.vn"
+    },
+    {
+        "stt": 74,
+        "gender": "chị",
+        "name": "Lê Thị Thu Trang",
+        "email": "tranglttsb614"
+    },
+    {
+        "stt": 75,
+        "gender": "chị",
+        "name": "Phạm Diễm Thắm",
+        "email": "thamptdsb62466@fpt.edu.vn"
+    },
+    {
+        "stt": 76,
+        "gender": "chị",
+        "name": "Nguyễn Hồng Thu Phương",
+        "email": "phuongnhtse63330@fpt.edu.vn"
+    },
+    {
+        "stt": 81,
+        "gender": "chị",
+        "name": "Phan Thị Diễm Thắm",
+        "email": "thamptdsb61466@fpt.edu.vn"
+    },
+    {
+        "stt": 82,
+        "gender": "chị",
+        "name": "Trương Thị Mỹ Hiếu",
+        "email": "HieuTTMSB61462@fpt.edu.vn"
+    },
+    {
+        "stt": 105,
+        "gender": "chị",
+        "name": "Phạm Thị Hồng Huê",
+        "email": "Huepthss130190@fpt.edu. vn"
+    },
+    {
+        "stt": 256,
+        "gender": "bạn",
+        "name": "Phạm Nguyễn Trà My",
+        "email": "pntramy3601@gmail.com.vn"
+    },
+
+]
+const FEV_CORE_TEAM_TBSNX = [{
+        "stt": 1,
+        "name": "Phạm Lê Tấn Bầu",
+        "email": "phamletanthinh999@gmail.com",
+        "gender": "anh"
+    },
+    {
+        "stt": 2,
+        "name": "Phạm Hữu Nghĩa",
+        "email": "nghiapham2704@gmail.com",
+        "gender": "anh"
+    },
+    {
+        "stt": 3,
+        "name": "Trần Phú Sơn",
+        "email": "phuson2809@gmail.com",
+        "gender": "anh"
+    },
+    {
+        "stt": 4,
+        "name": "Vũ Ngọc Thu Giang",
+        "email": "thugiangvungoc@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 5,
+        "name": "Bùi Bình An",
+        "email": "119anbui@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 6,
+        "name": "Võ Thị Thảo Vân",
+        "email": "vothithaovan933@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 7,
+        "name": "Nguyễn Thanh Ngọc",
+        "email": "thanhngoc.nguyen0306@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 8,
+        "name": "Trần Thị Phương Thảo",
+        "email": "pthaopt0603@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 9,
+        "name": "Quách Nguyễn Toàn Phát",
+        "email": "phatduiga@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 10,
+        "name": "Phan Hạnh Trang",
+        "email": "trangphanhanh118@gmail.com",
+        "gender": "bạn"
+    },
+    {
+        "stt": 11,
+        "name": "Ngô Minh Hưng",
+        "email": "ngominhhung208@gmail.com",
+        "gender": "anh"
+    },
+    {
+        "stt": 12,
+        "name": "Võ Đăng Khoa",
+        "email": "khoamixx@gmail.com",
+        "gender": "anh"
+    },
+    {
+        "stt": 13,
+        "name": "Võ Xuân Đạt",
+        "email": "vxdat13@gmail.com",
+        "gender": "anh"
+    }, {
+        "stt": 14,
+        "name": "Đỗ Dương Tâm Đăng",
+        "email": "jacob15072000@gmail.com",
+        "gender": "anh "
+    }
+]
+const FIX_STYLE = [{
+    "stt": 14,
+    "name": "Đỗ Dương Tâm Đăng",
+    "email": "jacob15072000@gmail.com",
+    "gender": "anh "
+}, {
+    "stt": 1,
+    "name": "Phạm Lê Tấn Bầu",
+    "email": "phamletanthinh999@gmail.com",
+    "gender": "anh"
+}]
+module.exports = {
+    FEV_MEMBER_LIST,
+    FEV_CORE_TEAM_TBSNX,
+    FIX_STYLE
+}
